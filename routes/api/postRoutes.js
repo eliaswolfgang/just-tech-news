55345631078f80afc50a96ee28f6aaf929d77444
Ian E Von Fange <wolfgangvonfange@gmail.com>
2022-03-14 04:53:48 -0500

    Added Comments to db and routes to API

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
   Post.findAll({
@@ -18,6 +18,14 @@ router.get('/', (req, res) => {
     ],
     order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username'],
@@ -51,6 +59,14 @@ router.get('/:id', (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username'],
